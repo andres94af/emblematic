3c8f5e4911e180adab875adf19f11c9d726e5a62
Andres Fernandez <andres94.af@gmail.con>
2023-10-19 12:51:54 +0200

    creado servidor de eureka base y configurados los microservicios

---
+++ emblematic-eureka-service/src/test/java/com/emblematic/eurekaservice/EmblematicEurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emblematic.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmblematicEurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
