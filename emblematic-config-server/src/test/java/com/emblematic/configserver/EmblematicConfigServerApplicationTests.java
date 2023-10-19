d508ba02f91d13d6ee6f5a9093f583cd4a4a98a7
Andres Fernandez <andres94.af@gmail.con>
2023-10-19 17:10:05 +0200

    creando servidor de configuracion

---
+++ emblematic-config-server/src/test/java/com/emblematic/configserver/EmblematicConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emblematic.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmblematicConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
